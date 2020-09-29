818781e133d5cc75b7769865e148130fc900431e
Anwesh43 <anweshthecool0@gmail.com>
2020-09-30 01:21:39 +0530

    Canvas extension method

--- arrowlinecreatorview/src/main/java/com/anwesh/uiprojects/arrowlinecreatorview/ArrowLineCreatorView.kt
+++ arrowlinecreatorview/src/main/java/com/anwesh/uiprojects/arrowlinecreatorview/ArrowLineCreatorView.kt
@@ -22,11 +22,46 @@ val colors : Array<Int> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.02f / 4
 val strokeFactor : Int = 90
-val sizeFacotor : Float = 3.9f
-val mainLineFactor : Float = 2.9f
+val sizeFactor : Float = 3.9f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 60f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawArrowLineCreator(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val mainSize : Float = size * Math.sin(rot * Math.PI / 180).toFloat()
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2, 0f)
+        rotate(rot * sf2)
+        drawLine(0f, 0f, 0f, size * sf1, paint)
+        restore()
+    }
+    save()
+    translate(0f, h / 2 * (1 - sf4))
+    drawLine(0f, 0f, 0f, -mainSize * sf3, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawALCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArrowLineCreator(scale, w, h, paint)
+}
+
