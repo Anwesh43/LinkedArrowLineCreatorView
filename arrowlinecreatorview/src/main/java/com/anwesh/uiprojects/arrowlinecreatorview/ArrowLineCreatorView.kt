f4669a5ec6c49113d0e0ec50895622ab1e304a17
Anwesh43 <anweshthecool0@gmail.com>
2020-09-30 02:34:05 +0530

    adding ArrowLineCreator class

--- arrowlinecreatorview/src/main/java/com/anwesh/uiprojects/arrowlinecreatorview/ArrowLineCreatorView.kt
+++ arrowlinecreatorview/src/main/java/com/anwesh/uiprojects/arrowlinecreatorview/ArrowLineCreatorView.kt
@@ -169,4 +169,27 @@ class ArrowLineCreatorView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArrowLineCreator(var i : Int, val state : State = State()) {
+
+        private var curr : ALCNode = ALCNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
