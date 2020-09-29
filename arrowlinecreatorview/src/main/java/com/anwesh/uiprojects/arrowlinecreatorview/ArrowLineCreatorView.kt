8ac39eed389cf78d07dcdb5b00b0f69f124218e8
Anwesh43 <anweshthecool0@gmail.com>
2020-09-30 01:00:30 +0530

    adding constants and libraries

--- arrowlinecreatorview/src/main/java/com/anwesh/uiprojects/arrowlinecreatorview/ArrowLineCreatorView.kt
+++ arrowlinecreatorview/src/main/java/com/anwesh/uiprojects/arrowlinecreatorview/ArrowLineCreatorView.kt
@@ -3,3 +3,25 @@ package com.anwesh.uiprojects.arrowlinecreatorview
 /**
  * Created by anweshmishra on 30/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#009688",
+        "#3F51B5",
+        "#FFC107",
+        "#4CAF50"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / 4
+val strokeFactor : Int = 90
+val sizeFacotor : Float = 3.9f
+val mainLineFactor : Float = 2.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
