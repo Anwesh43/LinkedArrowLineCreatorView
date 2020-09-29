ebfd4acc51c78662f4c8fac57d602eb717aea395
Anwesh43 <anweshthecool0@gmail.com>
2020-09-30 02:40:46 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedarrowlinecreatorview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedarrowlinecreatorview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedarrowlinecreatorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.arrowlinecreatorview.ArrowLineCreatorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ArrowLineCreatorView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
