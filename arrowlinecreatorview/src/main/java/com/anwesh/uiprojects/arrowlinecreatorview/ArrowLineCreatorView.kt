2169ff0eb4d6a917354b5167d6ec464cec582498
Anwesh43 <anweshthecool0@gmail.com>
2020-09-30 01:26:13 +0530

    adding State class

--- arrowlinecreatorview/src/main/java/com/anwesh/uiprojects/arrowlinecreatorview/ArrowLineCreatorView.kt
+++ arrowlinecreatorview/src/main/java/com/anwesh/uiprojects/arrowlinecreatorview/ArrowLineCreatorView.kt
@@ -79,4 +79,24 @@ class ArrowLineCreatorView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += dir * scGap
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
+    }
 }
